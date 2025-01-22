592d1c3a9fa3384350f579938298fd3cbfa5b439
sjhub <sj.hub9796@gmail.com>
2025-01-22 23:46:31 +0900

    Update CraftPlayerMixin.java

--- arclight-common/src/main/java/io/izzel/arclight/common/mixin/bukkit/CraftPlayerMixin.java
+++ arclight-common/src/main/java/io/izzel/arclight/common/mixin/bukkit/CraftPlayerMixin.java
@@ -2,12 +2,16 @@ package io.izzel.arclight.common.mixin.bukkit;
 
 import com.google.common.base.Preconditions;
 import io.izzel.arclight.common.bridge.core.network.common.DiscardedPayloadBridge;
+import io.izzel.arclight.common.mod.server.ArclightServer;
 import io.netty.buffer.Unpooled;
+import net.minecraft.network.Utf8String;
 import net.minecraft.network.protocol.common.ClientboundCustomPayloadPacket;
 import net.minecraft.network.protocol.common.custom.DiscardedPayload;
 import net.minecraft.resources.ResourceLocation;
+import org.apache.logging.log4j.core.util.StringEncoder;
 import org.bukkit.craftbukkit.v.entity.CraftPlayer;
 import org.bukkit.event.player.PlayerRegisterChannelEvent;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.messaging.StandardMessenger;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
@@ -32,6 +36,21 @@ public abstract class CraftPlayerMixin extends CraftEntityMixin {
         channel = StandardMessenger.validateAndCorrectChannel(channel);
         if (this.channels.add(channel)) {
             this.server.getPluginManager().callEvent(new PlayerRegisterChannelEvent((CraftPlayer) (Object) this, channel));
+        }
+    }
+
+    /**
+     * @author sj-hub9796
+     * @reason
+     */
+    @Overwrite
+    public void sendPluginMessage(Plugin source, String channel, byte[] message) {
+        StandardMessenger.validatePluginMessage(this.server.getMessenger(), source, channel, message);
+        if (((CraftPlayer) (Object) this).getHandle().connection != null) {
+            if (this.channels.contains(channel)) {
+                ResourceLocation id = ResourceLocation.parse(StandardMessenger.validateAndCorrectChannel(channel));
+                this.sendCustomPayload(id, message);
+            }
         }
     }
 
